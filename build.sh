c70fa3a0c884f9b41c3fc50f97d2d0852e9b63d4
kellen-cl <kellyally968@gmail.com>
2025-12-17 20:21:27 +0300

    Update build.sh and requirements.txt for Render deployment

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Install latest pip and build tools
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
